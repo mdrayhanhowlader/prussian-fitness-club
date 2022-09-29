e893cc895c22a1f3af89b827a0835056297728b6
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-09-29 20:54:45 +0600

    shop container class added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                
+            </div>
+            <div className="cart-container"></div>
+        </div>
+    );
+};
+
+export default Shop;
